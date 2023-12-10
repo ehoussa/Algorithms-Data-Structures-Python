04ea07e760076c567b12f356a0b9e2e3d2f48011
El houssaine Bakchich <ehoussachich@gmail.com>
2023-12-10 20:10:09 +0100

    easy

--- Valid Anagram/Valid_Anagram.py
+++ Valid Anagram/Valid_Anagram.py
@@ -2,6 +2,6 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         str1 = ''.join(sorted(s))
         str2 = ''.join(sorted(t))
-        if str1 != str2:
+        if str2 != str1:
             return False
         return True
